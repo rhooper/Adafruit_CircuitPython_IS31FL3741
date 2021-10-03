46b4dab807f1a0cb1d542e817161207d4cbf4ef4
Rose Hooper <rhooper@toybox.ca>
2021-10-03 17:05:12 -0400

    refactor (also speeds up by a little, too)

--- adafruit_is31fl3741/adafruit_rgbmatrixqt.py
+++ adafruit_is31fl3741/adafruit_rgbmatrixqt.py
@@ -26,6 +26,8 @@ Implementation Notes
 # imports
 from . import IS31FL3741
 
+REMAP = bytes([8, 5, 4, 3, 2, 1, 0, 7, 6])
+
 
 class Adafruit_RGBMatrixQT(IS31FL3741):
     """Supports the ISSI IS31FL3741 eval board"""
@@ -35,6 +37,28 @@ class Adafruit_RGBMatrixQT(IS31FL3741):
 
     @staticmethod
     def pixel_addrs(x, y):
+        """Calculate the RGB offsets into the device array for x,y pixel"""
+        col = x
+        row = REMAP[y]
+
+        if row <= 5:
+            if col < 10:
+                offset = 0x1E * row + col * 3
+            else:
+                offset = 0xB4 + 0x5A + 9 * row + (col - 10) * 3
+        else:
+            if col < 10:
+                offset = 0xB4 + (row - 6) * 0x1E + col * 3
+            else:
+                offset = 0xB4 + 0x5A + 9 * row + (col - 10) * 3
+
+        if col == 12 or col % 2 == 1:  # odds + last col
+            # r=2, g=1, b=0
+            return offset + 2, offset + 1, offset
+        return offset, offset + 2, offset + 1
+
+    @staticmethod
+    def pixel_addrs_old(x, y):
         """Calulate the RGB offsets into the device array for x,y pixel"""
         col = x
         row = y
@@ -70,3 +94,12 @@ class Adafruit_RGBMatrixQT(IS31FL3741):
             b_off = 1
 
         return (offset + r_off, offset + g_off, offset + b_off)
+
+
+def test():
+    for x in range(13):
+        for y in range(9):
+            a = Adafruit_RGBMatrixQT.pixel_addrs(x, y)
+            b = Adafruit_RGBMatrixQT.pixel_addrs_old(x, y)
+            if a != b:
+                print("ERROR", x, y, a, b)
